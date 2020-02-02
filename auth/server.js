348fb7ce81fce9b1b65fb18f3e6856b81b5ffb51
Nsuliman <nsuliman88@gmail.com>
2020-02-02 16:26:29 +0200

    server starts

---
+++ auth/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// use logger 
+const loggerReq = require('./logger.js');
+app.use(loggerReq);
+
+// errors 
+const err404 = require('../middleware/404.js');
+const err500 = require('../middleware/500.js');
+
+app.use(err404);
+app.use(err500);
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+// Server listening 
+module.exports = {
+    server : app,
+    start : port => {
+      let PORT = port || process.env.PORT || 8080;
+      app.listen(PORT , ()=> console.log(`The App Is a live and Listening on Port No.${PORT}`));
+    },
+  };
