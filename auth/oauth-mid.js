b9e74fc153f81513f1b017ffc0bf77d1f509e97e
Nsuliman <nsuliman88@gmail.com>
2020-02-08 21:30:44 +0200

    github app created

--- auth/oauth-mid.js
+++ auth/oauth-mid.js
@@ -1,3 +1,18 @@
 'use strict';
 
+// make request to github API to do the handshake with it 
 const superagent = require('superagent');
+const users = require('./users.js');
+
+// url that github tells us to make request to it to get the access token 
+const tokenAccessURL = 'https://github.com/login/oauth/access_token';
+
+// API accessing 
+const remoteAccessAPI = 'https://api.github.com/user';
+
+// inside .env file 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+// create an app on github through this route  
+const SERVER_API = process.env.SERVER_API;
+
